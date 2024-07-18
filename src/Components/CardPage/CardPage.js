0df6fb55514038b15addc35f563c708c2d69a523
Varvara Korableva <v.korableva1990@yandex.ru>
2024-07-18 14:25:58 +0300

    changed card page styles

--- src/Components/CardPage/CardPage.js
+++ src/Components/CardPage/CardPage.js
@@ -3,6 +3,9 @@ import { useParams, Link } from 'react-router-dom';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext'
 import './CardPage.css'
 import * as Api from '../../Api/Api'
+import {LanguageContext} from '../../contexts/TranslationContext'
+import choose from '../../const/CardPageData'
+import noPictures from '../../images/nopictures.png'
 
 function CardPage({ allImages, deleteMyAd, selectedItem, getItemById, addToFavorites, isLoggin, favoriteItems, deleteFromFavorites }) {
     let { item_id } = useParams();
@@ -11,6 +14,19 @@ function CardPage({ allImages, deleteMyAd, selectedItem, getItemById, addToFavor
     const favorite_collector_id = currentUser.user_id
 
     const [userInfo, setUserInfo] = React.useState([])
+
+    const { language } = React.useContext(LanguageContext)
+
+    const { en, rus, hebrew } = choose;
+  
+    let translatedContext = '';
+    if (language === 'en') {
+      translatedContext = en;
+    } else if (language === 'rus') {
+      translatedContext = rus;
+    } else if (language === 'hebrew') {
+      translatedContext = hebrew;
+    }
 
     useEffect(() => {
         getItemById(item_id);
@@ -48,36 +64,42 @@ function CardPage({ allImages, deleteMyAd, selectedItem, getItemById, addToFavor
         <section className="cardPage-section">
             <div className="cardPage-main-container">
                 <div className="cardPage-container">
-                    {/*<div className="cardPage-img-container">
-                        <img src={image[0].location} alt={selectedItem[0].title} className='cardPage-image'/>
-                    </div>*/}
+
+
+                <div className="cardPage__main-pic-wrapper">
+                    {image.location?
+                        <img className="cardPage__main-pic" alt = {selectedItem.title} src={image[0].location}></img>
+                    : 
+                        <img className="cardPage__no-pic" alt = 'no pic' src={noPictures}></img>
+                    }
+                </div>
 
                     <div className="cardPage-info-container">
-                        <div>
-                            <p>Name: <span className="cardPage-info">{selectedItem[0].title}</span></p>
-                            <p>Price: <span className="cardPage-info">{selectedItem[0].price}</span></p>
-                            <p>City: <span className="cardPage-info">{selectedItem[0].city}</span></p>
+                        <div className="cardPage-info-wrapper">
+                            <p className="cardPage-info-title">{translatedContext.name}: <span className="cardPage-info">{selectedItem[0].title}</span></p>
+                            <p className="cardPage-info-title">{translatedContext.price}: <span className="cardPage-info">{selectedItem[0].price}</span></p>
+                            <p className="cardPage-info-title">{translatedContext.city}: <span className="cardPage-info">{selectedItem[0].city}</span></p>
                         </div>
 
                         <div className="cardPage__info-about-user">
-                            <Link to={`/users/${selectedItem[0].owner_id}`} className="cardPage-link">See all user ads &rarr;</Link>                       
+                            <Link to={`/users/${selectedItem[0].owner_id}`} className="cardPage-link">{translatedContext.seeAllUserAds} &rarr;</Link>                       
                         </div>
                         {
                             isLoggin?
                                 <div className="cardPage__btn-container">
                                     {currentUser.user_id === selectedItem[0].owner_id ?
-                                        <button className='cardPage__write-message-btn' onClick={hundleDeleteMyAd}>Delete ad</button>
+                                        <button className='cardPage__write-message-btn' onClick={hundleDeleteMyAd}>{translatedContext.deleteBtn}</button>
                                     
                                     :
                                         <>
                                         {isLiked? 
                                     
-                                           <button onClick={hundleDeleteFromFavorites} className='cardPage__favorite-btn'>Delete from favorites</button>
+                                           <button onClick={hundleDeleteFromFavorites} className='cardPage__favorite-btn'>{translatedContext.deleteFromFavBtn}</button>
                                         :
-                                           <button onClick={handleAddToFavorites} className='cardPage__favorite-btn'>Add to favorites</button>
+                                           <button onClick={handleAddToFavorites} className='cardPage__favorite-btn'>{translatedContext.addToFavBtn}</button>
                                         }
                                     
-                                        <button className='cardPage__write-message-btn'>Write a message</button>
+                                        <button className='cardPage__write-message-btn'>{translatedContext.writeAMessageBtn}</button>
                                         </>
                                     }
                                 </div>
@@ -90,10 +112,10 @@ function CardPage({ allImages, deleteMyAd, selectedItem, getItemById, addToFavor
                 {selectedItem[0].description.length <= 0 ? 
                 <></> 
                 :
-                <p>
-                    <span className="cardPage-info">Description:</span>
-                    {selectedItem[0].description}
-                </p>
+                <div className="cardPage-description-container">
+                    <p className="cardPage-info">{translatedContext.description}:</p>
+                    <p className="cardPage-info-description">{selectedItem[0].description}</p>
+                </div>
                 }
             </div>
         </section>
